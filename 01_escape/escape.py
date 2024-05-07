be09cd286406c61aa1a70e15a03b961037619f4a
Dana Goosen <klymentovska@gmail.com>
2024-05-07 17:01:08 +0200

    add draw_line method

---
+++ 01_escape/escape.py
@@ -0,0 +1,14 @@
+def escaped(position):
+    x = int(position[0])
+    x = int(position[1])
+
+    return x < -35 or x > 35 or y < -35 or y > 35
+
+def draw_line():
+    angle = 0
+    step = 5
+    t = turtle.Turtle()
+
+    while not escaped(t.position()):
+        t.left(angle)
+        t.forward(step)
---
+++ 01_escape/hello_turtle.py
@@ -0,0 +1,34 @@
+import turtle
+
+def draw_bag():
+    turtle.shape('turtle')
+    turtle.pen(pencolor='brown', pensize=5)
+    turtle.penup()
+    turtle.goto(-35, 35)
+    turtle.pendown()
+    turtle.right(90)
+    turtle.forward(70)
+    turtle.left(90)
+    turtle.forward(70)
+    turtle.left(90)
+    turtle.forward(70)
+
+def escaped(position):
+    x = int(position[0])
+    x = int(position[1])
+
+    return x < -35 or x > 35 or y < -35 or y > 35
+
+def draw_line():
+    angle = 0
+    step = 5
+    t = turtle.Turtle()
+
+    while not escaped(t.position()):
+        t.left(angle)
+        t.forward(step)
+
+if __name__ == '__main__':
+    turtle.setworldcoordinates(-70., -70., 70., 70.)
+    draw_bag()
+    turtle.mainloop()
