bf50148ea422dc6ed4f7565e8fb1fd14c02358e9
Dana Goosen <klymentovska@gmail.com>
2024-05-08 07:32:26 +0200

    fixed typos and imports

--- 01_escape/escape.py
+++ 01_escape/escape.py
@@ -1,6 +1,10 @@
+import turtle
+import argparse
+import inspect
+
 def escaped(position):
     x = int(position[0])
-    x = int(position[1])
+    y = int(position[1])
 
     return x < -35 or x > 35 or y < -35 or y > 35
 
@@ -12,3 +16,38 @@ def draw_line():
     while not escaped(t.position()):
         t.left(angle)
         t.forward(step)
+
+def draw_bag():
+    turtle.shape('turtle')
+    turtle.pen(pencolor='brown', pensize=5)
+    turtle.penup()
+    turtle.goto(-35, 35)
+    turtle.pendown()
+    turtle.right(90)
+    turtle.forward(70)
+    turtle.left(90)
+    turtle.forward(70)
+    turtle.left(90)
+    turtle.forward(70)
+
+if __name__ == '__main__':
+    fns = { "line": draw_line }
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--function",
+        choices = fns,
+        help = "One of " + ', '.join(fns.keys()))
+    parser.add_argument("-n", "--number", default=50, type=int, help="How many?")
+    args = parser.parse_args()
+
+    try:
+        f = fns[args.function]
+        turtle.setworldcoordinates(-70., -70., 70., 70.)
+        draw_bag()
+        turtle.hideturtle()
+        if len(inspect.getfullargspec(f).args) == 1:
+            f(args.number)
+        else:
+            f()
+        turtle.mainloop()
+    except KeyError:
+        parser.print_help()
